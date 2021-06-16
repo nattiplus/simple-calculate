e7879fbcd4ad0571a77b5d9e4d0df699a95d5b6c
sangitgreenwich <68804474+sangitgreenwich@users.noreply.github.com>
2021-06-16 11:49:16 +0700

    Modified Server and HTML file

--- server.js
+++ server.js
@@ -33,9 +33,45 @@ const server = http.createServer((req, res) => {
         </body>
         </html>`);
         break;
-        default:
+        case "/simplecalculate.html":
             res.writeHead(200);
             res.end(htmlfileread);
+            break;
+        default:
+            res.writeHead(200);
+            var q = url.parse(req.url, true);
+            console.log(req.url);
+            var result = dt.calc(Number.parseInt(q.query.a), Number.parseInt(q.query.b),q.query.p);
+            res.end(`
+            <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Simple Calculate Web</title>
+                </head>
+                <body>
+                    <h1 style="text-align: center;">Welcome To A Simple Calculator !!!</h1>
+                    <h3 style="text-align: center;">Please! Input value for a, b and p</h3>
+                    <form method="GET" action="https://simplecalculateapp.herokuapp.com/calculate" style="display: block; text-align: center;">
+                        <br>
+                        <label for="value-a">a: </label>
+                        <input type="text" id="value-a" name="a" value=""/>
+                        <br><br>
+                        <label for="value-b">b: </label> 
+                        <input type="text" id="value-b" name="b" value=""/>
+                        <br><br>
+                        <label for="value-p">p: </label> 
+                        <input type="text" id="value-p" name="p" value=""/>
+                        <br><br>
+                        <input type="submit" value="Calculate Now" onclick="formdata()"/><br>
+                        </form>
+                        <form>
+                            <label for="value-result">Result: </label>
+                            <input type="text" id="value-result" value="${result}"/>
+                        </form>
+                </body>
+                </html>`)
             break;
     }
 })
